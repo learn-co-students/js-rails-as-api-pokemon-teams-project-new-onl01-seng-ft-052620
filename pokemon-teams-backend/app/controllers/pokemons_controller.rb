5b7f5e57863e4e5624337e44093f12eb10a8b292
SWFullPotential <swfullpotential@gmail.com>
2020-09-06 18:14:04 -0600

    Done.

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,2 +1,25 @@
 class PokemonsController < ApplicationController
+    def show 
+        set_pokemon
+        render json: pokemon, include: [:trainer]
+    end 
+
+    def randomPokemon()
+        name = Faker::Name.first_name
+        species = Faker::Games::Pokemon.name 
+        Pokemon.create(nickname: name, species: species)
+    end
+
+    def destroy 
+        set_pokemon
+        trainer = pokemon.trainer 
+        pokemon.destroy 
+        render json: trainer, include: [:pokemons]
+    end
+    
+    private
+
+    def set_pokemon
+        pokemon = Pokemon.find_by_id(params[:id])
+    end
 end
--- pokemon-teams-backend/config/routes.rb
+++ pokemon-teams-backend/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :pokemons
   resources :trainers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post 'trainers/:id' => 'trainers#addPokemon'
   
 end
