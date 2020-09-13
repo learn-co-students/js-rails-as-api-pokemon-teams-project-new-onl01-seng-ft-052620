66a8f3d38ec1b99c1faefbc8e99ed7e711398150
francepants <franceline90@gmail.com>
2020-09-13 15:11:52 -0500

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -50,10 +50,38 @@ const renderPokemon = (pokemon) => {
     ul.appendChild(li)
 }
 
-const createPokemon = () => {
+const createPokemon = (e) => {
+    e.preventDefault()
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({trainer_id: e.target.dataset.trainerId})
+    }
 
+    fetch(POKEMONS_URL, configObj)
+    .then(resp => resp.json())
+    .then(json => {
+        if (json.message) {
+            alert(json.message)
+        } else {
+            renderPokemon(json)
+        }
+    })
 }
 
-const deletePokemon = () => {
+const deletePokemon = (e) => {
+    e.preventDefault()
+    const configObj = {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+    }
 
+    fetch(`${POKEMONS_URL}/${e.target.dataset.pokemonId}`, configObj)
+    e.target.parentElement.remove()
 }
